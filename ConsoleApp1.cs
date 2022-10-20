cb83e53b1947b3b9dc324ee4bb60de68a675615b
C.Vengels <6281096+cvengels@users.noreply.github.com>
2022-10-21 00:21:47 +0200

    Create ConsoleApp1.cs

---
+++ ConsoleApp1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string messageFromSale = "";
+            Console.WriteLine("\n" + messageFromSale + "\n"); // evil code line
+            //                  ^^ 
+            // If you delete those both characters, the error module goes apeshit and loops forever until the RAM is full and the computer freezes up.
+            // Sometimes it has a different trigger, but these characters work every time.
+        }
+    }
+}
