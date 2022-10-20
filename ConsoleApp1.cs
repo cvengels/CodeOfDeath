32d32bd46b3a9f3fdcc289a48249f9d016eff74a
C.Vengels <6281096+cvengels@users.noreply.github.com>
2022-10-21 00:29:07 +0200

    Update ConsoleApp1.cs

--- ConsoleApp1.cs
+++ ConsoleApp1.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
             string messageFromSale = "";
             Console.WriteLine("\n" + messageFromSale + "\n"); // evil code line
             //                  ^^ 
-            // If you delete those both characters, the error module goes apeshit and loops forever until the RAM is full and the computer freezes up.
+            // If you delete these both characters, the error module goes apeshit and loops forever until the RAM is full and the computer freezes up.
             // Sometimes it has a different trigger, but these characters work every time.
         }
     }
